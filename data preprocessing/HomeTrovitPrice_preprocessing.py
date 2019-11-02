8ba998f2c2f8ea7b6d1128139947f1d4b1918cfb
FatinNabilah1 <40049342+FatinNabilah1@users.noreply.github.com>
2019-11-02 23:01:41 +0800

    Update HomeTrovitPrice_preprocessing.py

--- data preprocessing/HomeTrovitPrice_preprocessing.py
+++ data preprocessing/HomeTrovitPrice_preprocessing.py
@@ -8,6 +8,9 @@ transformation and cleaning process
 """
 
 import pandas as pd
+import numpy as np
+
+num = 0
 
 dataframe = pd.read_csv(r'D:\GitHub\WXD7005\HomeTrovit\data extraction\Cheras_HomeTrovit_raw.csv')
 dataframe_clean = dataframe
@@ -29,12 +32,21 @@ dataframe_clean['Floor Size or Property Size sq. feet'] = pd.to_numeric(datafram
 #remove a row or a column from a dataframe which has a NaN or no values in it
 dataframe_clean.dropna(subset=['Floor Size or Property Size sq. feet'], inplace=True)
 
+a = dataframe_clean['Address or Zone']
+cheras = a.str.contains('Cheras')
+kuala_lumpur = a.str.contains('Kuala Lumpur')
+selangor = a.str.contains('Selangor')
+dataframe_clean['Address or Zone'] = pd.np.where(cheras, 'Cheras',
+                                               pd.np.where(kuala_lumpur, 'Kuala Lumpur',
+                                                        pd.np.where(selangor, 'Selangor','NaN')))
+
+
+#get the Price per sq. feeta
+dataframe_clean['Price/sq. feet'] = dataframe_clean['Price'] / dataframe_clean['Floor Size or Property Size sq. feet']
+
 #drop unneccessary attributes ; 'Url', 'Image', 'Source Info'
 dataframe_clean.drop(['Url', 'Image', 'Source Info'], axis=1, inplace=True)
 dataframe_clean.drop(dataframe_clean.columns[0], axis=1, inplace=True)
-
-#get the Price per sq. feet
-dataframe_clean['Price/sq. feet'] = dataframe_clean['Price'] / dataframe_clean['Floor Size or Property Size sq. feet']
 
 ##Save clean data to csv file
 dataframe_clean.to_csv('Cheras_HomeTrovit_clean.csv')
