b49730dbf1f6b9af041d22e36b37b3c0d958a5e3
FatinNabilah1 <40049342+FatinNabilah1@users.noreply.github.com>
2019-11-03 10:00:05 +0800

    Update HomeTrovitPrice_preprocessing.py

--- data preprocessing/HomeTrovitPrice_preprocessing.py
+++ data preprocessing/HomeTrovitPrice_preprocessing.py
@@ -23,38 +23,38 @@ dataframe_clean['Price'] = pd.to_numeric(dataframe_clean['Price'], errors='coerc
 dataframe_clean.dropna(subset=['Price'], inplace=True)
 
 #Convert “No of Bedroom Available” to numeric values
-dataframe_clean['No of Bedroom Available'] = pd.to_numeric(dataframe_clean['No of Bedroom Available'], errors='coerce')
+dataframe_clean['no_of_bedroom'] = pd.to_numeric(dataframe_clean['No of Bedroom Available'], errors='coerce')
 #remove a row or a column from a dataframe which has a NaN or no values in it
-dataframe_clean.dropna(subset=['No of Bedroom Available'], inplace=True)
+dataframe_clean.dropna(subset=['no_of_bedroom'], inplace=True)
 
 #Convert “Floor Size or Property Size sq. feet” to numeric values
-dataframe_clean['Size sq. feet'] = pd.to_numeric(dataframe_clean['Floor Size or Property Size sq. feet'], errors='coerce')
+dataframe_clean['Square(Ft)'] = pd.to_numeric(dataframe_clean['Floor Size or Property Size sq. feet'], errors='coerce')
 #remove a row or a column from a dataframe which has a NaN or no values in it
-dataframe_clean.dropna(subset=['Floor Size or Property Size sq. feet'], inplace=True)
+dataframe_clean.dropna(subset=['Square(Ft)'], inplace=True)
 
 #Reassign the Zone
 a = dataframe_clean['Address or Zone']
 cheras = a.str.contains('Cheras')
 kuala_lumpur = a.str.contains('Kuala Lumpur')
 selangor = a.str.contains('Selangor')
-dataframe_clean['Address or Zone'] = pd.np.where(cheras, 'Cheras',
+dataframe_clean['Zone'] = pd.np.where(cheras, 'Cheras',
                                                pd.np.where(kuala_lumpur, 'Kuala Lumpur',
-                                                        'Selangor'))
+                                                        pd.np.where(selangor, 'Selangor','')))
 
 #remove a row or a column from a dataframe which has a NaN or no values in it
-dataframe_clean.dropna(subset=['Address or Zone'], inplace=True)
-
+dataframe_clean['Zone'].replace('', np.nan, inplace=True)
+dataframe_clean.dropna(subset=['Zone'], inplace=True)
+ 
 #get the Price per sq. feet
-dataframe_clean['Price/sq. feet'] = dataframe_clean['Price'] / dataframe_clean['Floor Size or Property Size sq. feet']
+dataframe_clean['Price/Square(Ft)'] = dataframe_clean['Price'] / dataframe_clean['Square(Ft)']
 
 
 #drop unneccessary attributes ; 'Url', 'Image', 'Source Info'
 dataframe_clean.drop(['Url', 'Image', 'Source Info'], axis=1, inplace=True)
-dataframe_clean.drop(dataframe_clean.columns[0], axis=1, inplace=True)
 
 #filter for data property size < 3000 sq feet and price < 2000000
 #other remove from dataframe
-dataframe_clean = dataframe_clean[dataframe_clean['Floor Size or Property Size sq. feet'] < 3000]
+dataframe_clean = dataframe_clean[dataframe_clean['Square(Ft)'] < 3000]
 dataframe_clean = dataframe_clean[dataframe_clean['Price'] < 2000000]
 dataframe_clean.reset_index(inplace=True, drop=True)
 
@@ -62,4 +62,4 @@ dataframe_clean.reset_index(inplace=True, drop=True)
 dataframe_clean.drop_duplicates(keep=False, inplace=True)
 
 ##Save clean data to csv file
-dataframe_clean.to_csv('Cheras_HomeTrovit_clean.csv')
+dataframe_clean.to_csv('Cheras_HomeTrovit_clean.csv', index=False)
